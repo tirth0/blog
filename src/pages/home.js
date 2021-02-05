a85f434c2d7d5566680ef5913c06c09992e8ef15
tirth0 <tirtharajsengupta@gmail.com>
2021-02-05 20:17:45 +0530

    initial commit

---
+++ src/pages/home.js
@@ -0,0 +1,42 @@
+import * as React from "react";
+import { Link } from "react-router-dom";
+import ProgressiveImage from "react-progressive-image";
+
+const Home = ({ imageDetails, image }) => (
+  <>
+    <main>
+      <div className='container'>
+        <div className='row center'>
+          <div className='image-container'>
+            <div
+              className='thumbnail'
+              ref={image}
+              style={{
+                width: imageDetails.width,
+                height: imageDetails.height,
+              }}>
+              <div className='frame'>
+                <Link to={`/model/yasmeen-tariq`}>
+                  <ProgressiveImage
+                    src={require("../images/vampire.jpg")}
+                    placeholder={require("../images/vampire.jpg")}>
+                    {(src) => <img src={src} alt='vampire' />}
+                  </ProgressiveImage>
+                </Link>
+              </div>
+            </div>
+            <div className='information'>
+              <div className='title'>Yasmeen Tariq</div>
+              <div className='location'>
+                <span>28.538336</span>
+                <span>-81.379234</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  </>
+);
+
+export default Home;
